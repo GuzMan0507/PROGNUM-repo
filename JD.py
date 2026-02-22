4079e0c420369e9ec5192504ee81fb6e4bd7ace4
GuzMan0507 <gzmp05@gmail.com>
2026-02-22 23:26:03 +0100

    Add files via upload

---
+++ JD.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import math
+Y=int(input("Year:"))
+M=int(input("Month:"))
+D=float(input("Day:"))
+
+JD = 367*Y -7*(Y+(M+9)//12)//4 - 3*((Y+(M-9)//7)//100 + 1)//4 + (275*M)//9 + D + 1721029-0.5
+
+print(f"The date in Julian Date is:{JD}")
+
+
+# In[ ]:
+
+
+
+
